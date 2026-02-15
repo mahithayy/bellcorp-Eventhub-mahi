4c94ac729bd3782fb4b41998bc8028ce25859f5c
mahithayy <hollamahitha@gmail.com>
2026-02-15 14:14:41 +0530

    set up cookie parser

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/user", userRoutes);
